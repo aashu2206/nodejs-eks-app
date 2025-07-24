a0a2707cab9025521af12c22f35cf61df8e321aa
aashu2206 <86254509+aashu2206@users.noreply.github.com>
2025-07-24 15:13:17 +0530

    server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 
+// Read SSL certificate and key
 const options = {
-  key: fs.readFileSync('./certs/key.pem'),
-  cert: fs.readFileSync('./certs/cert.pem')
+  key: fs.readFileSync(path.join(__dirname, 'certs', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'certs', 'cert.pem'))
 };
 
+// Create HTTPS server
 https.createServer(options, (req, res) => {
   res.writeHead(200);
   res.end("Hello from Node.js on HTTPS in EKS!");
 }).listen(443, () => {
-  console.log("Server running on https://localhost:443");
+  console.log("✅ Server running on https://localhost:443");
 });
